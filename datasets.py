61d88790492e7c7afdd8ccb78d01fb1a4868aa59
Mehdi Rahim <mehdi.rahim@cea.fr>
2015-01-19 12:55:27 +0100

    added fdg pet fetching

--- datasets.py
+++ datasets.py
@@ -10,7 +10,7 @@ from sklearn.datasets.base import Bunch
 
 
 
-def set_base_dir():
+def set_rs_fmri_base_dir():
     """ BASE_DIR could be on disk4t or on FREECOM
     """
     
@@ -24,11 +24,26 @@ def set_base_dir():
 	        raise OSError('Data not found !')
     return base_dir
 
+
+def set_fdg_pet_base_dir():
+    """ BASE_DIR could be on disk4t or on FREECOM
+    """
+    
+    base_dir = '/disk4t/mehdi/data/ADNI_baseline_fdg_pet'
+    if not os.path.isdir(base_dir):
+        base_dir = '/media/FREECOM/Data/ADNI_baseline_fdg_pet'
+        if not os.path.isdir(base_dir):
+            base_dir = '/media/mr243268/FREECOM/Data/ADNI_baseline_fdg_pet'
+	    if not os.path.isdir(base_dir):
+		base_dir = ''
+	        raise OSError('Data not found !')
+    return base_dir
+
 def fetch_adni_rs_fmri():
     """ Returns paths of ADNI resting-state fMRI
     """
     
-    BASE_DIR = set_base_dir()
+    BASE_DIR = set_rs_fmri_base_dir()
     subject_paths = sorted(glob.glob(os.path.join(BASE_DIR, 's[0-9]*')))
     excluded_subjects = np.loadtxt(os.path.join(BASE_DIR,
                                                 'excluded_subjects.txt'),
@@ -38,11 +53,34 @@ def fetch_adni_rs_fmri():
                                              'description_file.csv'))
     func_files = []
     dx_group = []
+    subjects = []
     for f in subject_paths:
         _, subject_id = os.path.split(f)
         if not subject_id in excluded_subjects:
             func_files.append(glob.glob(os.path.join(f, 'func', 'twr*.nii'))[0])
             dx_group.append( \
             s_description[s_description.Subject_ID == subject_id[1:]].DX_Group_x.values[0])
-    return Bunch(func=func_files, dx_group=dx_group)
+            subjects.append(subject_id[1:])
+    return Bunch(func=func_files, dx_group=dx_group, subjects=subjects)
 
+
+def fetch_adni_fdg_pet():
+    """ Returns paths of ADNI FDG-PET
+    """
+
+    BASE_DIR = set_fdg_pet_base_dir()
+    s_description = pd.read_csv(os.path.join(BASE_DIR, 'description_file.csv'))
+    
+    pet_files = []
+    dx_group = []       
+    subjects = []
+    for idx, row in s_description.iterrows():
+        pet_files.append(glob.glob(os.path.join(BASE_DIR,
+                                                'I' + str(row.Image_ID),
+                                                'wI*.nii'))[0])
+        dx_group.append(row['DX_Group'])
+        subjects.append(row['Subject_ID'])
+        
+    return Bunch(pet=pet_files, dx_group=dx_group, subjects=subjects)
+    
+    
