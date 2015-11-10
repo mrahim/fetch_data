990f7d0143d212a261b8569a94c15f1b768063c0
Mehdi Rahim <rahim.mehdi@gmail.com>
2015-11-10 19:01:03 +0100

    small fixes

--- fetch_data/datasets.py
+++ fetch_data/datasets.py
@@ -502,7 +502,7 @@ def fetch_atlas(atlas_name):
                              'atlas_canica_141_rois.nii.gz')
     elif atlas_name == 'tvmsdl':
         atlas = os.path.join(CACHE_DIR, 'atlas',
-                             'atlas_tv_msdl.nii.gz')
+                             'tvmsdl_abide.nii.gz')
     elif atlas_name == 'kmeans':
         atlas = os.path.join(CACHE_DIR, 'atlas',
                              'atlas_kmeans.nii.gz')
